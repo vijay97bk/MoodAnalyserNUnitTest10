3fa3d1ab79e3fa2eaffba93c2296bcaa60b230fa
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-12 18:38:32 +0530

    [vijay]FirstCommit

---
+++ MoodAnalyserNUnitTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyserNUnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome message");
+        }
+    }
+}
